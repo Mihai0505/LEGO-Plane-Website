e02a1d043a840dfacece3cec4e5b847b1ea9f660
Mihai0505 <118223998+Mihai0505@users.noreply.github.com>
2026-04-20 23:09:12 +0300

    Update 3d_view_script.js

--- js/3d_view_script.js
+++ js/3d_view_script.js
@@ -73,6 +73,14 @@ scene.add( light );
 //This adds controls to the camera, so we can rotate / zoom it with the mouse
 if (objToRender === "plane") {
   controls = new OrbitControls(camera, renderer.domElement);
+
+
+  controls.rotateSpeed = 2.5;
+  controls.zoomSpeed = 2;
+  controls.panSpeed = 2;
+
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
 }
 
 //Render the scene
