10118979f34b2daf30c9664189baff412f233c8a
Mihai0505 <118223998+Mihai0505@users.noreply.github.com>
2026-04-20 23:41:59 +0300

    Update 3d_view_script.js

--- js/3d_view_script.js
+++ js/3d_view_script.js
@@ -74,13 +74,9 @@ scene.add( light );
 if (objToRender === "plane") {
   controls = new OrbitControls(camera, renderer.domElement);
 
-
   controls.rotateSpeed = 2.5;
-  controls.zoomSpeed = 10;
+  controls.zoomSpeed = 5;
   controls.panSpeed = 2;
-
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.12;
 }
 
 //Render the scene
